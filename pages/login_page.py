32c537a70428e87b3e48af63abf41628ed2989c6
Andrey Pavlov <andrey@eskapandr.localdomain>
2026-01-19 18:12:03 +0400

    login_page.py added

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.current_url, "Login is not presented in current url"
+
+    def should_be_login_form(self):
+        self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert True
