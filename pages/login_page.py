da53b878fb26d91d9ba2271cb8ffe08b98425b3c
Andrey Pavlov <andrey@eskapandr.localdomain>
2026-01-21 16:54:01 +0400

    fixed LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.current_url, "Login is not presented in current url"
+        assert "login" in self.browser.current_url, "Login is not presented in current url"
 
     def should_be_login_form(self):
         self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
--- test_product_page_2.py
+++ test_product_page_2.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
@@ -11,4 +12,6 @@ def test_guest_should_see_login_link_on_product_page(browser):
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
-    assert page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
